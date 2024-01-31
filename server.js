451b4a80e2b9f1dd36c8ccf29fc65d5e743f6158
ztanvir <zahirulopel@gmail.com>
2024-01-31 12:20:29 +0600

    Convert app from nodejs to express js

--- server.js
+++ server.js
@@ -1,49 +1,29 @@
-const http = require("http");
-const fs = require("fs/promises");
+const express = require("express");
+const path = require("path");
+const app = express();
+const morgan = require("morgan");
 const host = "127.0.0.1";
 const port = 8000;
 
-const server = http.createServer(async (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  if (req.url == "/") {
-    try {
-      const content = await fs.readFile(__dirname + "/index.html");
-      res.statusCode = 200;
-      res.end(content);
-    } catch (error) {
-      res.writeHead(500);
-      res.end(error);
-    }
-  } else if (req.url == "/about") {
-    try {
-      const content = await fs.readFile(__dirname + "/about.html");
-      res.statusCode = 200;
-      res.end(content);
-    } catch (error) {
-      res.writeHead(500);
-      res.end(error);
-    }
-  } else if (req.url == "/visit") {
-    try {
-      const content = await fs.readFile(__dirname + "/visit.html");
-      res.statusCode = 200;
-      res.end(content);
-    } catch (error) {
-      res.writeHead(500);
-      res.end(error);
-    }
-  } else {
-    try {
-      const content = await fs.readFile(__dirname + "/404.html");
-      res.statusCode = 200;
-      res.end(content);
-    } catch (error) {
-      res.writeHead(500);
-      res.end(error);
-    }
-  }
+app.use(morgan("dev"));
+const unknownEndPoint = (req, res) => {
+  res.sendFile(path.join(__dirname, "pages/404.html"));
+};
+
+app.get("/", (req, res) => {
+  console.log("File path-", path.join(__dirname, "pages/index.html"));
+  res.sendFile(path.join(__dirname, "pages/index.html"));
 });
 
-server.listen(port, host, () => {
+app.get("/visit", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages/visit.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages/about.html"));
+});
+
+app.use(unknownEndPoint);
+
+app.listen(port, () => {
   console.log(`Server is running on https"://${host}:${port}`);
 });
